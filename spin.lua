465d686bc63697a8794ebbb4076c5ddcd329a0f6
aslan90101 <aslan0aslanov0ezz@gmail.com>
2024-11-16 15:02:44 +0300

    Update spin.lua

--- spin.lua
+++ spin.lua
@@ -1,1 +1,16 @@
-
+--Made by KH0NL
+--Spinner Script
+ 
+--Makes you spin very fast and being able to fling people.
+local Speed = 50000 -- How fast you want to spin
+local LocalPlayer = game.Players.LocalPlayer
+local Char = workspace:FindFirstChild(LocalPlayer.Name)
+local RootPart = Char:WaitForChild("HumanoidRootPart")
+local Torque = Instance.new("Torque")
+Torque.Enabled = true
+Torque.Torque = Vector3.new(0,Speed,0)
+Torque.Parent = RootPart
+local Attachment = Instance.new("Attachment")
+Attachment.Parent = RootPart
+Attachment.Position = RootPart.Position
+Torque.Attachment0 = Attachment
